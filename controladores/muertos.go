3c7c36b0648ea9b2c0c4ca5faffa4bbca43a937a
Jesus Gonzalez <gonzalezlrjesus@gmail.com>
2020-03-29 20:55:12 -0400

    error declaracion time

--- controladores/muertos.go
+++ controladores/muertos.go
@@ -15,7 +15,7 @@ var GetCasosMuertos = func(w http.ResponseWriter, r *http.Request) {
 	var errorCasos error
 	fmt.Println("Valor total:", modelos.PaisesCasosMuertos)
 	fmt.Println("len(modelos.PaisesCasosMuertos):", len(modelos.PaisesCasosMuertos))
-	fmt.Println("modelos.PaisesCasosMuertos[0].ActualizacionDia.Before(time.Now().UTC())", modelos.PaisesCasosMuertos[0].ActualizacionDia.Before(time.Now().UTC()))
+
 	switch { // missing expression means "true"
 
 	case len(modelos.PaisesCasosMuertos) == 0 || !(modelos.PaisesCasosMuertos[0].ActualizacionDia.Before(time.Now().UTC())):
