d470fcac6f654394639a6f422dc4cc43ae84804b
Jesus Gonzalez <gonzalezlrjesus@gmail.com>
2020-03-29 20:51:39 -0400

    fix controladores declaracion

--- controladores/muertos.go
+++ controladores/muertos.go
@@ -3,23 +3,25 @@ package controladores
 import (
 	"covid-19API/modelos"
 	"covid-19API/utils"
+	"fmt"
 	"net/http"
 	"time"
-	"fmt"
 )
 
 var urlCovid19Deaths = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
 
 // GetCasosMuertos depending on query
 var GetCasosMuertos = func(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Valor total:" , modelos.PaisesCasosMuertos)
-	fmt.Println("len(modelos.PaisesCasosMuertos):" , len(PaisesCasosMuertos))
-	fmt.Println("modelos.PaisesCasosMuertos[0].ActualizacionDia.Before(time.Now().UTC())" , modelos.PaisesCasosMuertos[0].ActualizacionDia.Before(time.Now().UTC()))
+	var errorCasos error
+	fmt.Println("Valor total:", modelos.PaisesCasosMuertos)
+	fmt.Println("len(modelos.PaisesCasosMuertos):", len(modelos.PaisesCasosMuertos))
+	fmt.Println("modelos.PaisesCasosMuertos[0].ActualizacionDia.Before(time.Now().UTC())", modelos.PaisesCasosMuertos[0].ActualizacionDia.Before(time.Now().UTC()))
 	switch { // missing expression means "true"
 
 	case len(modelos.PaisesCasosMuertos) == 0 || !(modelos.PaisesCasosMuertos[0].ActualizacionDia.Before(time.Now().UTC())):
-		modelos.PaisesCasosMuertos, error := utils.UpdateVariable(urlCovid19Deaths, modelos.PaisesCasosMuertos)
-		if error != nil {
+
+		modelos.PaisesCasosMuertos, errorCasos = utils.UpdateVariable(urlCovid19Deaths, modelos.PaisesCasosMuertos)
+		if errorCasos != nil {
 			response := utils.Message("Internal Server Error!")
 			utils.Respond(w, response, 500)
 			return
